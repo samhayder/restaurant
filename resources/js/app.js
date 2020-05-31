10dd5a26347f3dc3027fe01dd5a46bf1d6602cd0
samhayder <sams.seul@gmail.com>
2020-05-31 22:58:56 +0600

    add navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  //Navbar Toggler
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("change");
+  });
+
+  //Navbar Position Sticky
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 718) {
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    } else {
+      $(".navbar").removeClass("navbar-background");
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
   //Ripples by Water effect in Homepage
   $("#header, #homepage_top").ripples({
     dropRadius: 05,
